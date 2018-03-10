039c5672f6eb727c8fb16814dea465de0378ddc1
Scott Young <scott.t.young1@gmail.com>
2018-03-10 13:04:41 -0700

    projects page

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -4,14 +4,37 @@ import ScrollableAnchor from 'react-scrollable-anchor';
 
 class Projects extends Component {
     
-        constructor() {
-        super();
+        constructor(props) {
+        super(props);
         this.state= {
-            
+            featuresClosed: false,
+            clonesClosed: false,
+            appsClosed: false,
         }
+
+        this.closeFeatures = this.closeFeatures.bind(this);
+        this.openClones = this.openClones.bind(this);
+        this.openApps = this.openApps.bind(this);
     }
-      
-    
+
+    closeFeatures() {
+        this.setState({
+            featuresClosed: !this.state.featuresClosed
+        })
+    }
+
+    openClones() {
+        this.setState({
+            clonesClosed: !this.state.clonesClosed
+        })
+    }
+
+    openApps() {
+        this.setState({
+            appsClosed: !this.state.appsClosed
+        })
+    }
+
     render() {
 
         return(
@@ -20,14 +43,33 @@ class Projects extends Component {
                 <ScrollableAnchor id={'projects'}><div></div></ScrollableAnchor>
                     <div className="projectContainer">
                 <div className="projectsTitle">Projects</div>
-                        <div className="featuresParent">
-                            <div className="features">All Projects</div>
-                            <div className="features">Featured Websites</div>
-                             <div className="features">Website Clones</div>
-                            <div className="features">Applications</div>
+                        <div className="workMenuParent">   
+                            <div id="features"onClick={this.closeFeatures}>Featured Websites</div>
+                            <div id="features" onClick={this.openApps}>Applications</div>
+                            <div id="features" onClick={this.openClones}>Website Clones</div>
                         </div>
 
-                        <div className="displayArea">
+                        <div className="allParent">
+                            <div className={this.state.featuresClosed ? 'close features' : 'features'}>
+                                <div className="featuresParent">
+                                    <div className="websites">Wolf Creek</div>
+                                    <div className="websites">Restore</div>
+                                    <div className="websites">Page Consulting</div>
+                                </div>
+                            </div>
+                            <div className={this.state.appsClosed ? 'close apps' : 'apps'}>
+                                <div className="featuresParent">
+                                    <div className="websites">Weather App</div>
+                                    <div className="websites">ToDo App</div>
+                                    <div className="websites">No Database App</div>
+                                </div>
+                            </div>
+                            <div className={this.state.clonesClosed ? 'close clones' : 'clones'}>
+                                <div className="featuresParent">
+                                    <div className="websites">Big Marlin Clone</div>
+                                    <div className="websites">Rocket Media Clone</div>
+                                </div>
+                            </div>
                         </div>
                     
                 </div> 
